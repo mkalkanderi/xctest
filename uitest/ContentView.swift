40c7bcd3748d034658a60e5434979397a31c9183
Mohammed Alkandari <mohammedkalkandari8@gmail.com>
2021-08-28 14:00:33 +0300

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Mohd on 28/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Mohd on 28/8/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
