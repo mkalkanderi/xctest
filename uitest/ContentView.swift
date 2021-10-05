52c32a29c9581a95f9e9a9514091fde77dfa7d7b
Mohammed Alkandari <mohammedkalkandari8@gmail.com>
2021-10-05 22:06:42 +0300

    second commit

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -1,4 +1,4 @@
-//
+//-
 //  ContentView.swift
 //  uitest
 //
@@ -8,12 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK:- var
+    @State private var firstCard = "card2"
+    @State private var secondCard = "card2"
+    @State private var thirdCard = "card2"
+    
+    
+    @State private var score = 0
+   
+    
+    //MARK:- body
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background").ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Text("Slots!")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(firstCard)
+                    Spacer()
+                    Image(secondCard)
+                    Spacer()
+                    Image(thirdCard)
+                    Spacer()
+                    
+                }.padding()
+                Spacer()
+                Button(action: {
+                    
+                    let firstRand = Int.random(in: 2...14)
+                    let secondRand = Int.random(in: 2...14)
+                    let thirdRand = Int.random(in: 2...14)
+                    //update card
+                    firstCard = "card" + String(firstRand)
+                    secondCard = "card" + String(secondRand)
+                    thirdCard = "card" + String(thirdRand)
+                    
+                    
+                    
+                    //update score
+                    if firstRand == secondRand && firstRand == thirdRand {
+                        score += 5000
+                    } else {
+                        score -= 10
+                    }
+                    
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack{
+                    Text("Score: \(score)")
+                }
+                
+                Spacer()
+                
+                
+            }
+        }
     }
 }
 
+//MARK:- Previews
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
